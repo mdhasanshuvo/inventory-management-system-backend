d787615f844d6dcdc43884382e67ab2341737e8f
Mohammed Hasan <mdhasanshuvo220@gmail.com>
2026-04-01 17:00:23 +0600

    chore: add centralized request validation middleware

--- src/routes/activityRoutes.js
+++ src/routes/activityRoutes.js
@@ -1,14 +1,13 @@
-import express from "express";
-import { getRecentActivities } from "../controllers/activityController.js";
-
-const router = express.Router();
-
-/**
- * @swagger
- * /activity:
- *   get:
- *     summary: Get latest activity logs
- */
-router.get("/", getRecentActivities);
-
+import express from "express";
+import { getRecentActivities } from "../controllers/activityController.js";
+const router = express.Router();
+
+/**
+ * @swagger
+ * /activity:
+ *   get:
+ *     summary: Get latest activity logs
+ */
+router.get("/", getRecentActivities);
+
 export default router;
